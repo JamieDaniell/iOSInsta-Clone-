098bcc8fe94321293cfa34b68412d002b91fed3d
JamieDaniell <J@mieDaniell.com>
2016-11-23 20:45:35 +0000

    login post feed follow

---
+++ ParseStarterProject/FeedTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FeedTableViewCell.swift
+//  Insta(Clone)
+//
+//  Created by James Daniell on 21/11/2016.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    @IBOutlet weak var postedImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
